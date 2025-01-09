0c114166bf62c0ee87f67d1e8cccc0449c89ccc5
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-09 18:29:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function insert_Row() {
     //Write your code here
-  
+  let doc=document.getElementById("sampleTable");
+  doc.innerHTML=`
+        <tr>
+		<td>New cell1</td> 
+		<td>New cell2</td>
+		</tr> 
+  `+doc.innerHTML;
   
 }
