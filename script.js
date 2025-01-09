c2b7c50c0c91e025efd5242af6b554e11a630fb5
KartikeyJoshi916 <95592393+KartikeyJoshi916@users.noreply.github.com>
2025-01-09 18:37:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ function insert_Row() {
     //Write your code here
   let doc=document.getElementById("sampleTable");
   doc.innerHTML=
-        <tr>
+        `<tr>
 		<td>New Cell1</td> 
 		<td>New Cell2</td>
-		</tr> 
+		</tr>`
   +doc.innerHTML;
   
 }
